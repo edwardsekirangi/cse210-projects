77ffb888c01a23d605633f1b6715eb7f2fb4ba78
eddiedhin <sekirangi1@gmail.com>
2025-03-19 10:32:45 +0300

    fractions learning

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimal());
+
+        // Second instance
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimal());
+
+
+        //Third instance
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimal());
     }
 }
