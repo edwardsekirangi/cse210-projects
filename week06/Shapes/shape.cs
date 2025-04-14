74d93b471c54892e23beaad1b0481ea9ddbefbad
eddiedhin <sekirangi1@gmail.com>
2025-04-14 16:11:39 +0300

    shapes

---
+++ week06/Shapes/shape.cs
@@ -0,0 +1,26 @@
+using System;
+// Shape.cs
+public class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual float GetArea()
+    {
+        return 0;
+    }
+}
